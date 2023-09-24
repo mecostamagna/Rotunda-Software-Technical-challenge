521bf8df4206bbbddc541bcfe6b1860a420e71f4
TinchoCostamagna <mecostamagna@gmail.com>
2023-09-24 20:22:10 -0300

    modify url_parser solution
    Add the parsing logic to a different file.

--- exercises/url_parser/index.js
+++ exercises/url_parser/index.js
@@ -1,64 +1,7 @@
-const getVariablesAndNonVariableNames = (urlFormat) =>  {
-    const urlSections = urlFormat.split('/');
+import { getUrlObject } from './parser.js';
 
-    
-    const variableNames = [];
-    const nonVariableNames = [];
-
-    urlSections.forEach(element => {
-        if (element.substring(0, 1) === ':')
-        {
-            variableNames.push(element.substring(1, element.length));
-        }
-        else
-        {
-            nonVariableNames.push(element);
-         }
-    });
-
-    return { variableNames, nonVariableNames };
-} 
-const getParametersObject = (urlInstance) => { 
-    const parametersString = urlInstance.split('?')[1];
-    const paremetersSections = parametersString.split('&');
-    const parametersObject = {};
-
-    paremetersSections.forEach(element => {
-        const [key, value] = element.split('=');
-        parametersObject[key] = isNaN(value) ? value : Number(value);
-    })
-
-    return parametersObject;
-}
- 
-const getVariablesObject = (urlFormat, urlInstance) => {
-    const { variableNames, nonVariableNames } = getVariablesAndNonVariableNames(urlFormat)
-    const variablesObject = {}
-
-    const variablesString = urlInstance.split('?')[0];
-    const variablesAndNonVariables = variablesString.split('/');
-
-    const variablesValues = variablesAndNonVariables.filter(element => !nonVariableNames.includes(element));
-
-    for (i = 0; i < variableNames.length; i++)
-    {
-        variablesObject[variableNames[i]] = isNaN(variablesValues[i]) ? variablesValues[i] : Number(variablesValues[i]);
-
-    }
-
-    return variablesObject
-
-}
-
-const getUrlObject = (urlFormat, urlInstance) => { 
-    const variablesObject = getVariablesObject(urlFormat, urlInstance);
-    const parametersObject = getParametersObject(urlInstance);
-
-    return { ...variablesObject, ...parametersObject };
-}
-
-const urlFormat = ':apiVersion/:version/api/:collection/:id/';
-const urlInstance = '1/6/api/listings/3/bye?sort=desc&limit=10'
+const urlFormat = ':version/api/:collection/:id/';
+const urlInstance = '6/api/listings/3/bye?sort=desc&limit=10'
 
 
 const urlObject = getUrlObject(urlFormat, urlInstance);
